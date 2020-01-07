b294cc5119db9f0235884345190c55d97e46a163
ggpwnkthx <ibjessup@gmail.com>
2020-01-07 11:10:38 -0500

    Update add_group.sh

--- scripts/add_group.sh
+++ scripts/add_group.sh
@@ -3,7 +3,12 @@ if [ -z "$(cat /etc/group | grep ^$1:)" ]
 then
 	if [ -z "$(command -v addgroup)" ]
 	then
-		groupadd $1
+		if [ -f "/usr/sbin/addgroup" ]
+		then
+			/usr/sbin/addgroup $1
+		else
+			groupadd $1
+		fi
 	else
 		addgroup $1
 	fi
